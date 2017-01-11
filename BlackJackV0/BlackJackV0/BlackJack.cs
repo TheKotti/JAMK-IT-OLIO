427c12257dc10ddf81b874e4ff41303afbd02b0d
K8721 <K8721@ICT-421-WS-15>
2017-01-11 14:36:32 +0200

    Satunnainen numero, X lopettaa, kolmas muutos

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -8,27 +8,48 @@ namespace JAMK.IT
   {
     static void Main()
     {
-      int myNumber = 17;
-      int theirNumber;
-      System.Console.WriteLine("*** BlackJack! ***");
-      System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-      //reading and converting 
-      theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-      //comparing that given umber is valid
-      if (theirNumber < 1 || theirNumber > 21)
-      {
-        Console.WriteLine("The given number is out of limits, try again.");
-      }
-      else {
-        //comparing
-        if (theirNumber >= myNumber && theirNumber <= 21)
-        {
-          System.Console.WriteLine("You win.");
-        }
-        else {
-          System.Console.WriteLine("You lose.");
-        }
-      }
+
+           
+          
+          string theirNumber = "";
+            int temp;
+            while (true)
+            {
+                Random rnd = new Random();
+                int myNumber = rnd.Next(10, 22);
+                System.Console.WriteLine("*** BlackJack! ***");
+                System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+                //reading and converting 
+                theirNumber = System.Console.ReadLine();
+                //comparing that given umber is valid
+                if (!int.TryParse(theirNumber, out temp))
+                {
+                    if (theirNumber == "x" || theirNumber == "exit" || theirNumber == "X")
+                    {
+                        Environment.Exit(0);
+                    }
+                    Console.WriteLine("Ei näin");
+                    theirNumber = Console.ReadLine();
+                    int.TryParse(theirNumber, out temp);
+                }
+                while (temp < 1 || temp > 21)
+                {
+                    Console.WriteLine("The given number is out of limits, try again.");
+                    theirNumber = Console.ReadLine();
+                    int.TryParse(theirNumber, out temp);
+                }
+
+                //comparing
+                Console.WriteLine("Dealer's number: " + myNumber);
+                if (temp >= myNumber && temp <= 21)
+                {
+                    System.Console.WriteLine("You win. Play again.");
+                }
+                else
+                {
+                    System.Console.WriteLine("You lose. Play again.");
+                }
+            }
     }
   }
 }
